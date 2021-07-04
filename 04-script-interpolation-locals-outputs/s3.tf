e95f814caf30f65f20b65f224324e4b5fdae1d50
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-07-04 16:27:25 -0300

    Importando bucket criado manualmente na AWS

--- 04-script-interpolation-locals-outputs/s3.tf
+++ 04-script-interpolation-locals-outputs/s3.tf
@@ -4,9 +4,18 @@ resource "aws_s3_bucket" "this" {
 }
 
 resource "aws_s3_bucket_object" "this" {
-  bucket = aws_s3_bucket.this.bucket
-  key    = "config/${local.ip_filepath}"
-  source = local.ip_filepath
-  etag   = filemd5(local.ip_filepath)
+  bucket       = aws_s3_bucket.this.bucket
+  key          = "config/${local.ip_filepath}"
+  source       = local.ip_filepath
+  etag         = filemd5(local.ip_filepath)
   content_type = "application/json"
+}
+
+resource "aws_s3_bucket" "manual" {
+  bucket = "myawsbucket-456457"
+  tags = {
+    Criado = "04/07/2021"
+    Importado = "04/07/2021"
+    ManagedBy = "Terraform"
+  }
 }
